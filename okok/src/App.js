a9d26433be30208d72817489da43c3c3b55b8d56
hwchw316 <hwchw316@gmail.com>
2021-03-05 04:59:08 +0000

    first commit with templates

---
+++ okok/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+/**
+ * Create 2 new components - Header and MemeGenerator
+ * Header will only display things
+ * MemeGenerator will be calling to an API and holding on to data
+ * Each should be in their own file with
+ */
+
+function App() {
+    return (
+        <h1>Hello world!</h1>
+    )
+}
+
+export default App
